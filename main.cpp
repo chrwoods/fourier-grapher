022cea341bab414edc3032d1c6c495aa15a5a7da
Christopher Woods <chrisscottwoods@gmail.com>
2019-04-29 10:27:57 -0500

    Added sliders for scale and buttons to reset time and graph, added sorting to fourier

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@
 #define GL_SILENCE_DEPRECATION
 #endif
 #include <GLFW/glfw3.h>
+#include <bits/stdc++.h> //for sorting
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-#define TIME_STEP 0.01f
-
 //error handler for GLFW
 static void glfw_error_callback(int error, const char* description) {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+bool compareFourierElements(FourierElement f1, FourierElement f2){
+  return (f1.amp > f2.amp);
 }
 
 int main(int argc, char* argv[]) {
@@ -34,6 +37,9 @@ int main(int argc, char* argv[]) {
   for(int i = 0; i < 40; i++){
     fouriers.push_back(fourier(data, i + 1));
   }
+
+  //sort fourier
+  sort(fouriers.begin(), fouriers.end(), compareFourierElements);
   
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
@@ -64,9 +70,12 @@ int main(int argc, char* argv[]) {
 
     // Main loop
     float time = 0;
+    //ImVec2 prev_point(0, 0);
+    vector<ImVec2> points;
+    
     while (!glfwWindowShouldClose(window))
     {
-      if(time >= TAU) time = 0;
+      //if(time >= TAU) time = 0;
         // Poll and handle events (inputs, window resize, etc.)
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
         // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
@@ -86,38 +95,66 @@ int main(int argc, char* argv[]) {
         {
 	  ImGui::Begin("Fourier Graphing Window"); //Create window
 
-	  static float sz = 36.0f;
 	  static ImVec4 ccol = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
 	  static ImVec4 lcol = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
 	  static ImVec4 gcol = ImVec4(0.0f, 1.0f, 1.0f, 1.0f);
-	  
-	  //ImGui::DragFloat("Size", &sz, 0.2f, 2.0f, 72.0f, "%.0f");
+	  static float time_step = 0.0005f;
+	  //time_step = TAU / (float)fouriers.size();
+	  static int depth = fouriers.size();
 
+	  ImGui::DragInt("Depth", &depth, 1.0f, 1, fouriers.size(), "%d");
+	  ImGui::DragFloat("Speed", &time_step, 0.0001f, 0.0f, 0.01f, "%.4f");
+
+	  static float scalar = 0.07f; //scales down the grapher
+	  ImGui::DragFloat("Scale", &scalar, 0.01f, 0.01f, 1.0f, "%.2f");
+	  
 	  ImGui::ColorEdit3("Background Color", (float*)&clear_color); // Edit 3 floats representing a color
 	  ImGui::ColorEdit4("Circle Color", &ccol.x);
 	  ImGui::ColorEdit4("Line Color", &lcol.x);
 	  ImGui::ColorEdit4("Graph Color", &gcol.x);
+	  if(ImGui::Button("Reset Time")){
+	    time = 0;
+	    points.clear();
+	  }
+	  if(ImGui::Button("Clear Graph")){
+	    points.clear();
+	  }
 	  
 	  const ImVec2 p = ImGui::GetCursorScreenPos();
-	  float x = p.x + 4.0f, y = p.y + 16.0f, spacing = 8.0f;
-	  float th = 1.0f;
-	  ImGui::GetWindowDrawList()->AddCircle(ImVec2(x + sz*0.5f, y + sz*0.5f), sz*0.5f, ImColor(ccol), 20, th); x += sz + spacing;  // Circle
 	  
 	  ImGui::Text("Current time: %.3f", time);               // Display some text (you can use a format strings too)
 
-	  ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	  ImGui::Text("Running at %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-	  float offx = 640.0f + p.x; //x offset
-	  float offy = 400.0f + p.y; //y offset
+	  float centx = 570.0f + p.x; //center x
+	  float centy = 200.0f + p.y; //center y
+	  float destx;
+	  float desty;
 	  
-	  for(unsigned int i = 0; i < fouriers.size(); i++){
+	  for(unsigned int i = 0; i < (unsigned int)depth; i++){
 	    FourierElement fe = fouriers[i];
-	    float centx = offx + fe.x;
-	    float centy = offy + fe.y;
-	    float destx = centx + fe.amp * cos(TAU * fe.freq * time + fe.phase);
-	    float desty = centy + fe.amp * sin(TAU * fe.freq * time + fe.phase);
-	    ImGui::GetWindowDrawList()->AddCircle(ImVec2(centx, centy), fe.amp*0.5f, ImColor(ccol), i + 5, 1.0f); 
+	    destx = centx + fe.amp * scalar * cos(TAU * fe.freq * time + fe.phase);
+	    desty = centy + fe.amp * scalar * sin(TAU * fe.freq * time + fe.phase);
+	    ImGui::GetWindowDrawList()->AddCircle(ImVec2(centx, centy), fe.amp * scalar, ImColor(ccol), 50, 1.0f); 
 	    ImGui::GetWindowDrawList()->AddLine(ImVec2(centx, centy), ImVec2(destx, desty), ImColor(lcol), 1.0f);
+	    centx = destx;
+	    centy = desty;
+	  }
+
+	  /*if(prev_point.x != 0 && prev_point.y != 0){
+	    ImGui::GetWindowDrawList()->AddLine(prev_point, ImVec2(destx, desty), ImColor(gcol), 1.0f);
+	  }
+	  prev_point = ImVec2(destx, desty);*/
+	  points.push_back(ImVec2(destx, desty));
+	  for(unsigned int i = 1; i < points.size(); i++){
+	    ImGui::GetWindowDrawList()->AddLine(points[i - 1], points[i], ImColor(gcol), 1.0f);
+	  }
+	  
+	  //increment time
+	  time += time_step;
+	  if(time > TAU){
+	    time -= TAU;
+	    points.clear();
 	  }
 	  
 	  ImGui::End();
@@ -141,9 +178,6 @@ int main(int argc, char* argv[]) {
 
         glfwMakeContextCurrent(window);
         glfwSwapBuffers(window);
-
-	//increment time
-	time += TIME_STEP;
     }
 
     // Cleanup
