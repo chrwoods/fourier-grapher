67c538f0c86ec326a433e059e2bbeeb44eda75f1
Christopher Woods <chrisscottwoods@gmail.com>
2019-05-05 20:51:06 -0500

    Added in-between values

--- data_compare.cpp
+++ data_compare.cpp
@@ -26,5 +26,18 @@ int main(int argc, char* argv[]) {
     }
     cout << to_string(i) << ": expected: " << to_string(data[i][0]) << ", " << to_string(data[i][1]); 
     cout << ", calculated: " << to_string(x/2) << ", " << to_string(y/2) << endl;
+    //calculate the in-between
+    if(i == data.size() - 1) break;
+    cout << to_string(i) << ".5: expected: " << to_string((data[i][0] + data[i + 1][0]) / 2.0) << ", " << to_string((data[i][1] + data[i + 1][1]) / 2.0);
+    
+    x = 0;
+    y = 0;
+    for(unsigned int j = 0; j < fouriers.size(); j++){
+      FourierElement fe = fouriers[j];
+      x += fe.amp * cos(TAU * fe.freq * ((i + 0.5) / (double)data.size()) + fe.phase);
+      y += fe.amp * sin(TAU * fe.freq * ((i + 0.5) / (double)data.size()) + fe.phase);
+    }
+
+    cout << ", calculated: " << to_string(x/2) << ", " << to_string(y/2) << endl;
   }
 }
