4382ebe241e82602e8079b7dc02d1d3dfe0fd036
Christopher Woods <chrisscottwoods@gmail.com>
2019-05-05 20:44:14 -0500

    Created data_compare to test fourier calculated values against datapoints

---
+++ data_compare.cpp
@@ -0,0 +1,30 @@
+#include "fourier.cpp"
+#include "read_data.cpp"
+
+int main(int argc, char* argv[]) {
+  const char* filename = "data/diag_line.txt";
+  if(argc > 1) filename = argv[1];
+  
+  //fill data
+  vector<array<double, 2>> data;
+  data = get_data_from_file(filename);
+  
+  //fill fourier
+  vector<FourierElement> fouriers;
+  for(int i = -1 * data.size(); i <= (signed int)data.size(); i++){
+    if(i == 0) continue; //skips 0 so we don't have a circle that sits still
+    fouriers.push_back(fourier(data, i));
+  }
+
+  for(unsigned int i = 0; i < data.size(); i++){
+    double x = 0;
+    double y = 0;
+    for(unsigned int j = 0; j < fouriers.size(); j++){
+      FourierElement fe = fouriers[j];
+      x += fe.amp * cos(TAU * fe.freq * (i / (double)data.size()) + fe.phase);
+      y += fe.amp * sin(TAU * fe.freq * (i / (double)data.size()) + fe.phase);
+    }
+    cout << to_string(i) << ": expected: " << to_string(data[i][0]) << ", " << to_string(data[i][1]); 
+    cout << ", calculated: " << to_string(x/2) << ", " << to_string(y/2) << endl;
+  }
+}
